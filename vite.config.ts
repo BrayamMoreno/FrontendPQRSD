3d74ad58329cd8e97d4c4a955e730bdf16fb04bf
Brayam Stiven Moreno <112638896+BrayamMoreno@users.noreply.github.com>
2025-11-02 15:12:35 -0500

    Change server allowed hosts and port comment
    Updated server configuration with new allowed hosts.

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5137,
+    port: 5137, //cambiar por el puerto 
     host: true,
-    allowedHosts: ["springfood.studio"]
+    allowedHosts: ["dominio.ejemplo"] //cambiar por el nombre de su dominio
   },
   build: {
     minify: 'terser',
